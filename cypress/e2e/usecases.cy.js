998a079bc01b0d74d98ffe249fcc6b914a88d5bb
Michele Rodrigues <michelyrodrigues451@gmail.com>
2024-04-19 21:58:56 -0300

    initial commit

---
+++ cypress/e2e/usecases.cy.js
@@ -0,0 +1,26 @@
+/// <reference types= "cypress"/>
+
+describe ("teste lacrei e2e", ()=> {
+  beforeEach(()=>{
+    const email = "micheleee549@gmail.com";
+    const passwd = "1234567Mr.";
+    cy.login(email, passwd);
+  })
+  
+  it ("Buscar profissional", ()=>{
+cy.get ("#search").type (`Medico gay{enter}`);
+cy.url ().should("include", "/lista-de-profissionais/?q=Medico+gay");
+
+
+  })
+
+ it ("Editar perfil do usuário", ()=>{
+  cy.get ("a[href='/perfil/']").click()
+  cy.url ().should("include", "/perfil/")
+  cy.contains("button", "Editar dados").click()
+  cy.get ("#lastName").type(`{selectall}{backspace}Rodrigues`)
+  cy.wait(700)
+  cy.contains ("button", "Salvar").click()
+  cy.get("span[class='sc-be497de3-1 jmtbFE']").eq(0).contains ("Rodrigues")
+ })
+})
